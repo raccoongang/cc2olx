b55f9184cb0440094c9010e0240d64886d875e39
Myhailo Chernyshov <mykhailo.chernyshov@raccoongang.com>
2025-01-15 11:50:09 +0200

    refactor: [FC-0063] Block type processors are integrated into the script workflow

--- src/cc2olx/constants.py
+++ src/cc2olx/constants.py
@@ -2,7 +2,7 @@ OLX_STATIC_DIR = "static"
 OLX_STATIC_PATH_TEMPLATE = f"/{OLX_STATIC_DIR}/{{static_filename}}"
 WEB_RESOURCES_DIR_NAME = "web_resources"
 
-LINK_HTML = "<a href='{url}'>{text}</a>"
+LINK_HTML = '<a href="{url}">{text}</a>'
 YOUTUBE_LINK_PATTERN = r"youtube.com/watch\?v=(?P<video_id>[-\w]+)"
 CDATA_PATTERN = r"<!\[CDATA\[(?P<content>.*?)\]\]>"
 
